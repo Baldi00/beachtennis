b35cf9dbb0f3114b6a24fbbabb019d0de90013c3
Brankale <45881310+Brankale@users.noreply.github.com>
2021-03-17 21:43:07 +0100

    added Event class

---
+++ beachtennis/modules/models/Event.php
@@ -0,0 +1,38 @@
+<?php
+
+class Event {
+
+    private $id;
+    private $name;
+    private $startDate;
+    private $endDate;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getStartDate() {
+        return $this->startDate;
+    }
+
+    public function setStartDate($startDate) {
+        $this->startDate = $startDate;
+    }
+
+    public function getEndDate() {
+        return $this->endDate;
+    }
+
+    public function setEndDate($endDate) {
+        $this->endDate = $endDate;
+    }
+
+}
