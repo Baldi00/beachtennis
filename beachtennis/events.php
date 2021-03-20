fdd06955c9603d30222f607f7795729788da899e
Brankale <45881310+Brankale@users.noreply.github.com>
2021-03-20 11:25:42 +0100

    added navbar to events.php

--- beachtennis/events.php
+++ beachtennis/events.php
@@ -27,51 +27,41 @@
     <title>Beach Tennis</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="index.php">Beach Tennis</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="players.php">Tutti gli Iscritti</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="couples.php">Tutte le Coppie</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="events.php">Eventi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="exportCSV.php?source=events">Esporta eventi</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link" href="addEvent.php">Aggiungi</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="editEvents.php">Modifica</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="deleteEvent.php">Cancella</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+<?php require 'templates/navbar.php' ?>
+
+<div class="container">
 
     <h3 align="center" style="margin-top: 10px;">Eventi</h3>
-            
-    <?php
-        $numRow = $result->num_rows;
 
-        if($numRow == 0){
-            echo "<h4 style='margin: 20px; text-align: center;'>Nessun evento presente. Vai in \"Aggiungi evento\" per crearne uno</h4>";
-            echo "<div width='100%' align='center'><button class='btn btn-primary' onclick='window.location.href = \"addEvent.php\";'>Aggiungi evento</button></div>";
-        } else {
-            echo '  <table class="table table-striped">
+    <div>
+        <div class="content" style="float: left; margin: 1em;">
+            <a href="exportCSV.php?source=events">
+                <button type="button" class="btn btn-success">Esporta Eventi</button>
+            </a>
+        </div>
+
+        <div class="content" style="float: right; margin: 1em;">
+            <a href="addEvent.php">
+                <button type="button" class="btn btn-success">Aggiungi</button>
+            </a>
+            <a href="editEvents.php">
+                <button type="button" class="btn btn-warning" onclick="">Modifica</button>
+            </a>
+            <a href="deleteEvent.php">
+                <button type="button" class="btn btn-danger" onclick="">Cancella</button>
+            </a>
+        </div>
+    </div>
+
+    <?php
+    $numRow = $result->num_rows;
+
+    if($numRow == 0){
+        echo "<h4 style='margin: 20px; text-align: center;'>Nessun evento presente. Vai in \"Aggiungi evento\" per crearne uno</h4>";
+        echo "<div width='100%' align='center'><button class='btn btn-primary' onclick='window.location.href = \"addEvent.php\";'>Aggiungi evento</button></div>";
+    } else {
+        echo '  <table class="table table-striped">
                         <thead>
                             <tr style="text-align: center;">
                             <th scope="col">Nome Evento</th>
@@ -81,27 +71,30 @@
                             </tr>
                         </thead>
                         <tbody>';
-            for($i=0; $i<$numRow; $i++){
-                $line = mysqli_fetch_assoc($result);
-                echo "  <tr style='text-align: center'>
+        for($i=0; $i<$numRow; $i++){
+            $line = mysqli_fetch_assoc($result);
+            echo "  <tr style='text-align: center'>
                             <th scope='row'>".$line["eventName"]."</th>";
 
-                if($line["startDate"]=="")
-                    echo "  <td>Da definire</td>";
-                else
-                    echo "  <td>".$line["startDate"]."</td>";
+            if($line["startDate"]=="")
+                echo "  <td>Da definire</td>";
+            else
+                echo "  <td>".$line["startDate"]."</td>";
 
-                if($line["endDate"]=="")
-                    echo "  <td>Da definire</td>";
-                else
-                    echo "  <td>".$line["endDate"]."</td>";
+            if($line["endDate"]=="")
+                echo "  <td>Da definire</td>";
+            else
+                echo "  <td>".$line["endDate"]."</td>";
 
-                echo "      <td><a href='selectUnder.php?eventID=".$line["eventID"]."'>Visualizza</a></td>
+            echo "      <td><a href='selectUnder.php?eventID=".$line["eventID"]."'>Visualizza</a></td>
                         </tr>";
-            }
-            echo '      </tbody>
-                    </table>';
         }
+        echo '      </tbody>
+                    </table>';
+    }
     ?>
+
+</div>
+
 </body>
 </html>
