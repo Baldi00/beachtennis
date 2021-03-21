b77b988555967a79fe30138ba53f194020a32d10
Brankale <45881310+Brankale@users.noreply.github.com>
2021-03-21 21:27:53 +0100

    removed unused css
    leftovers from the previously removed buttons

--- beachtennis/index.php
+++ beachtennis/index.php
@@ -14,48 +14,6 @@
     <script src="public/unknown/js/popper.min.js"></script>
     <script src="public/bootstrap/js/bootstrap.min.js"></script>
 
-    <style type="text/css">
-
-        @media (max-width: 1200px) {
-          .divWebsite{
-                width: 98%;
-                margin-left: 1%;
-                margin-right: 1%;
-                margin-top: 20px;
-                float: left;
-                box-shadow: 0 .125rem .25rem rgba(0,0,0,.075);
-                transition-duration: 0.3s;
-                background-color: #e6f7ff;
-            }
-        }
-
-        @media (min-width: 1200px) {
-          .divWebsite{
-                width: 31.33%;
-                margin: 1%;
-                float: left;
-                box-shadow: 0 .125rem .25rem rgba(0,0,0,.075);
-                transition-duration: 0.3s;
-                background-color: #e6f7ff;
-            }
-        }
-
-        .divWebsite:hover {
-            transition-duration: 0.3s;
-            box-shadow: 0px 0px 10px rgba(0,0,0,.2);
-        }
-
-        .divSiteName{
-            width: 100%;
-            height: 100%;
-            text-align: center;
-            padding: 5px;
-            color: black;
-            font-weight: bold;
-        }
-
-    </style>
-
     <title>Beach Tennis</title>
 </head>
 <body>
